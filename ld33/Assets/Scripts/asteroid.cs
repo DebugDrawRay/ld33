3ebff3f8b890cdf483a17dc4c37768194f4e4c1a
Raymond John Grice <raymondjgrice@gmail.com>
2015-08-22 11:50:44 -0700

    Fixed monster velocity, working on monster states

--- ld33/Assets/Scripts/asteroid.cs
+++ ld33/Assets/Scripts/asteroid.cs
@@ -12,7 +12,7 @@ public class asteroid : MonoBehaviour
         float y = Random.Range(0f, 1f);
         float z = Random.Range(0f, 1f);
         Vector3 forceVect = new Vector3(x, y, z);
-        Debug.Log(forceVect * initialForce);
+
         GetComponent<Rigidbody>().velocity = forceVect * initialForce;
     }
 }
--- ld33/Assets/monster.cs
+++ ld33/Assets/monster.cs
@@ -6,18 +6,41 @@ public class monster : MonoBehaviour
     private delegate void stateContainer();
     private stateContainer currentState;
 
+    [Header("Status Control")]
+    public float health;
+    private float damageTaken;
+
     [Header("Target Control")]
     private player _player;
     public float followSpeed;
     public float followAccel;
 
     [Header("Patrol Control")]
+    public float timeToNewWp;
+    public float patrolTime;
+
+    private float maxPatrolTime;
+    private float maxTimeToNewWp;
+
     private Vector3 waypoint;
+
+    [Header("Chase Control")]
+    public float chaseDistance;
+    public float damageThreshold;
+
+    private bool hostileHit;
+    private bool inSight;
+
+    [Header("Collisions")]
+    public string[] hostileTags;
 
     //components
     private Rigidbody rigid;
-
     private gameController _gameController;
+
+    //state control
+    private bool patrolling;
+    private bool dormant;
 
     void Awake()
     {
@@ -32,7 +55,12 @@ public class monster : MonoBehaviour
     {
         _player = player.Instance;
         _gameController = gameController.Instance;
+
         waypoint = _gameController.getWaypoint();
+
+        maxTimeToNewWp = timeToNewWp;
+        maxPatrolTime = patrolTime;
+        patrolling = true;
     }
 
     void initializeComponents()
@@ -45,31 +73,104 @@ public class monster : MonoBehaviour
         stateMachine();
     }
 
-    void patrol()
+    void patrolController()
     {       
         followTarget(waypoint);
     }
-
-    void followTarget(Vector3 target)
+    
+    void patrolTimer()
     {
-        Vector3 direction = target - transform.position;
-        Vector3 targetVel = direction; //fix this, add control for velocity
-        Vector3 vel = Vector3.Lerp(rigid.velocity, targetVel, followAccel);
-        rigid.velocity = vel;
-    }
-    void patrolState()
-    {
-        patrol();
+        if(patrolTime > 0)
+        {
+            patrolTime -= Time.deltaTime;
+        }
+        else if(patrolTime <= 0)
+        {
+            patrolTime = maxPatrolTime;
+            patrolling = false;
+        }
     }
 
-    void chaseState()
+    void chaseController()
     {
         followTarget(_player.transform.position);
     }
 
+    float targetDist(GameObject target)
+    {
+        return Vector3.Distance(transform.position, target.transform.position);
+    }
+
+    void followTarget(Vector3 target)
+    {
+        timeToNewWp -= Time.deltaTime;
+
+        if (timeToNewWp <= 0)
+        {
+            waypoint = _gameController.getWaypoint();
+            timeToNewWp = maxTimeToNewWp;
+        }
+
+        Vector3 direction = target - transform.position;
+        Vector3 targetVel = direction; 
+        targetVel = targetVel.normalized * followSpeed;
+        Vector3 vel = Vector3.Lerp(rigid.velocity, targetVel, followAccel);
+        rigid.velocity = vel;
+    }
+
+    //collisions
+    void OnTriggerEnter(Collider hit)
+    {
+        foreach(string tag in hostileTags)
+        {
+            if(hit.gameObject.tag == tag)
+            {
+                health -= 10;
+                damageTaken += 10;
+                hostileHit = true;
+            }
+        }
+    }
+
+    void patrolState()
+    {
+        patrolController();
+        patrolTimer();
+    }
+
+    void chaseState()
+    {
+        chaseController();
+    }
+
     void stateMachine()
     {
-        currentState = patrolState;
+        if (dormant)
+        if(patrolling)
+        {
+            currentState = patrolState;
+
+            if (targetDist(_player.gameObject) <= chaseDistance)
+            {
+                patrolling = false;
+            }
+
+            if (hostileHit)
+            {
+                hostileHit = false;
+                patrolling = false;
+            }
+        }
+        else
+        {
+            currentState = chaseState;
+            if(damageTaken >= damageThreshold)
+            {
+                damageTaken = 0;
+                patrolling = true;
+            }
+        }
+        
         currentState();
     }
 }
